a02e0f63ed361fb69d5a8b151c25aee15d4bbab3
Maksym Hapon <maksym.hapon@proxet.com>
2022-07-29 16:32:23 +0100

    add router for users

---
+++ auth_service/router/user_router.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+from typing import List
+
+from auth_service.repository.user_repository import UserRepository
+from auth_service.router import schemas
+from auth_service.service.user_service import UserService
+
+user_router = FastAPI()
+
+
+@user_router.post("/users/", response_model=schemas.user.User, tags=["users"])
+async def create_user(user: schemas.user.UserDTO):
+    db_user = UserRepository.get_user_by_username(user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail=f"User with username {user.username} already registered")
+    return UserService.create_user(user)
+
+
+@user_router.get("/users/", response_model=List[schemas.user.User], tags=["users"])
+async def read_users(offset: int, limit: int):
+    return UserRepository.get_users(offset=offset, limit=limit)
+
+
+@user_router.get("/users/{username}", response_model=schemas.user.User, tags=["users"])
+async def read_user_by_username(username: str):
+    maybe_user = UserRepository.get_user_by_username(username)
+    if not maybe_user:
+        raise HTTPException(status_code=404, detail=f"User with username {username} not found")
+    return maybe_user
