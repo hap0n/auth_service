737ae6fb6d850ac05a82dde5d08b7d40675d329e
Maksym Hapon <maksym.hapon@proxet.com>
2022-07-29 16:43:08 +0100

    add main router with uvicorn server

--- auth_service/router/user_router.py
+++ auth_service/router/user_router.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import APIRouter, HTTPException
 from typing import List
 
 from auth_service.repository.user_repository import UserRepository
 from auth_service.router import schemas
 from auth_service.service.user_service import UserService
 
-user_router = FastAPI()
+user_router = APIRouter()
 
 
-@user_router.post("/users/", response_model=schemas.user.User, tags=["users"])
+@user_router.post("/", response_model=schemas.user.User, tags=["users"])
 async def create_user(user: schemas.user.UserDTO):
     db_user = UserRepository.get_user_by_username(user.username)
     if db_user:
@@ -16,12 +16,12 @@ async def create_user(user: schemas.user.UserDTO):
     return UserService.create_user(user)
 
 
-@user_router.get("/users/", response_model=List[schemas.user.User], tags=["users"])
+@user_router.get("/", response_model=List[schemas.user.User], tags=["users"])
 async def read_users(offset: int, limit: int):
     return UserRepository.get_users(offset=offset, limit=limit)
 
 
-@user_router.get("/users/{username}", response_model=schemas.user.User, tags=["users"])
+@user_router.get("/{username}/", response_model=schemas.user.User, tags=["users"])
 async def read_user_by_username(username: str):
     maybe_user = UserRepository.get_user_by_username(username)
     if not maybe_user:
